b6b4799f23fc2e9679597127c69c728e8f4f5cc3
facu <facuromero12312@gmail.com>
2022-12-08 10:54:04 -0300

    terminamos la primera version del proyecto

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const navMenu = document.querySelector(".nav-menu")
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("nav-menu_visible")
 
-    if (navMenu.classList.contains("nav-menu_visible")){
+    if (navMenu.classList.contains("nav-menu_visible")) {
         navToggle.setAttribute("aria-label","Cerrar menu");
-    } else{
+    } else {
         navToggle.setAttribute("aria-label","Abrir menu");
     }
-    }
-);
+    
+});
